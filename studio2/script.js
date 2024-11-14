7fe452f2257102ce69574f89e927b383d099c2bc
원예나 <won-yena@won-yenaui-MacBook-Air.local>
2024-11-14 02:02:45 -0800

    studio2

--- studio2/script.js
+++ studio2/script.js
@@ -8,29 +8,15 @@
             console.error("Element '.a' not found.");
             return;
         }
-    
+
         const sliderWidth = sliderContent.scrollWidth;
-    
         const cloned = sliderContent.cloneNode(true);
         cloned.className = "b";
         document.querySelector('.slider').appendChild(cloned);
-    
+
         document.documentElement.style.setProperty('--sliderwidth', `-${sliderWidth}px`);
         document.querySelector('.slider').classList.add("animate");
-
-        // Popup effect on image hover
-        const images = document.querySelectorAll('.image-container');
-        
-        images.forEach(function (image) {
-            image.addEventListener('mouseover', function () {
-                const description = image.querySelector('.description');
-                description.style.display = 'block';
-            });
-            
-            image.addEventListener('mouseout', function () {
-                const description = image.querySelector('.description');
-                description.style.display = 'none';
-            });
-        });
     });
+
 })();
+
