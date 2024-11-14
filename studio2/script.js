4acef21bb61f4b765cf065fee9da428bf174ad3b
원예나 <won-yena@won-yenaui-MacBook-Air.local>
2024-11-14 01:36:41 -0800

    studio2

--- studio2/script.js
+++ studio2/script.js
@@ -3,29 +3,21 @@
     console.log("reading js");
 
     window.addEventListener('load', function () {
-        'use strict';
         const sliderContent = document.querySelector('.a');
+        if (!sliderContent) {
+            console.error("Element '.a' not found.");
+            return;
+        }
     
-        // How wide is the original set of images?
-        const sliderWidth = sliderContent.offsetWidth;
+        const sliderWidth = sliderContent.scrollWidth;
     
-        // clone the set of images and assign them the class name of '.b'
         const cloned = sliderContent.cloneNode(true);
         cloned.className = "b";
-    
-        // add the clone to the DOM
         document.querySelector('.slider').appendChild(cloned);
     
-        //get the :root element
-        let root = document.querySelector(':root');
-    
-        // set the end of the left position based on the width of the slider
-        const endLeftPos = `-${sliderWidth}px`;    
-        root.style.setProperty('--sliderwidth', endLeftPos);
-    
-        //Add the animate class to start animating the slider
+        document.documentElement.style.setProperty('--sliderwidth', `-${sliderWidth}px`);
         document.querySelector('.slider').classList.add("animate");
     });
-
+    
 
 })();
