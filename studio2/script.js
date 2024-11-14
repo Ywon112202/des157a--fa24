d1c3c0218c627e629fd91f4b2039b53deb9ac91c
원예나 <won-yena@won-yenaui-MacBook-Air.local>
2024-11-14 04:25:24 -0800

    studio2

--- studio2/script.js
+++ studio2/script.js
@@ -11,15 +11,15 @@
 
         const sliderWidth = sliderContent.scrollWidth;
     
-        // Clone the slider content for the looping effect
+
         const cloned = sliderContent.cloneNode(true);
         cloned.className = "b";
         document.querySelector('.slider').appendChild(cloned);
 
-        // Set the necessary width for the slider container
+
         document.documentElement.style.setProperty('--sliderwidth', `${sliderWidth}px`);
 
-        // Apply animation class to start the slider
+
         document.querySelector('.slider').classList.add("animate");
     });
 })();
